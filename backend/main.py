8c74470ca119aaf51ffc96d776e5960d2bf8fe5f
Ibrahim Rasyid <thamrinihsan@gmail.com>
2026-08-10 19:30:43 +0700

    Feat: Create initial console app

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,25 @@
-def intro():
-    pass
+def print_trip_summary(destination, country, days, budget, currency, travel_month):
+    print(
+        """
+========================
+KelanaAI
+========================
+        """
+    )
+    print(f"Destination: {destination}")
+    print(f"Country: {country}")
+    print(f"Days: {days}")
+    print(f"Budget: {budget} USD")
+    print(f"Currency: {currency}")
+    print(f"Travel Month: {travel_month}")
+
 
 if __name__ == "__main__":
-    intro()
+    destination = input("Insert destination: ")
+    country = input("Insert country: ")
+    days = int(input("Insert days: "))
+    budget = float(input("Insert budget: "))
+    currency = input("Insert currency: ")
+    travel_month = input("Insert travel month: ")
+
+    print_trip_summary(destination, country, days, budget, currency, travel_month)
