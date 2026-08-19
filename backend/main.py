8907579a08362a23022969d43e8e695b48139268
Ibrahim Rasyid <thamrinihsan@gmail.com>
2026-08-19 23:23:03 +0700

    Feat: Add PostgreSQL persistence

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
 from services.trip_services import calculate_daily_budget, get_trip_category, get_transportation, get_travel_season
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+
+from models.trip import Trip
+from database import SessionLocal, init_db
+
+
 
 class TripRequest(BaseModel):
     destination     : str
@@ -9,6 +14,10 @@ class TripRequest(BaseModel):
     travel_style    : str
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 @app.get('/')
 def home():
@@ -28,14 +37,72 @@ def create_trip(request: TripRequest):
     daily_budget    = calculate_daily_budget(request.budget, request.days)
     rec_transport   = get_transportation(category)
 
-    return {
-        "destination"           : request.destination,
-        "days"                  : request.days,
-        "budget"                : request.budget,
-        "daily_budget"          : daily_budget,
-        "category"              : category,
-        "recommended_transport" : rec_transport,
-    }
+    trip = Trip(
+        destination     = request.destination,
+        days            = request.days,
+        budget          = request.budget,
+        category        = category,
+        daily_budget    = daily_budget
+    )
+
+    db = SessionLocal()
+    db.add(trip)
+    db.commit()
+    db.refresh(trip)
+    db.close()
+    return trip
+
+@app.get("/api/v1/trips")
+def list_trips():
+    db = SessionLocal()
+    trips = db.query(Trip).all()
+    db.close()
+
+    return trips
+
+@app.get("/api/v1/trips/{trip_id}")
+def get_trip(trip_id: int):
+    db = SessionLocal()
+    trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    db.close()
+    # handling not found
+    if trip is None:
+        raise HTTPException(status_code=404, detail=f"Trip with id {trip_id} not found")
+
+    return trip
+
+@app.delete('/api/v1/trips/{id}')
+def delete_trip(trip_id: int):
+    db = SessionLocal()
+    trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if trip is None:
+        db.close()
+        raise HTTPException(status_code=404, detail=f"Trip with id {trip_id} not found")
+    db.delete(trip)
+    db.close()
+
+    return trip
+
+@app.put('/api/v1/trips/{id}')
+def edit_trip(trip_id:int, request: TripRequest):
+    category        = get_trip_category(request.budget)
+    daily_budget    = calculate_daily_budget(request.budget, request.days)
+    rec_transport   = get_transportation(category)
+
+    db = SessionLocal()
+    trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if trip is None:
+        db.close()
+        raise HTTPException(status_code=404, detail=f"Trip with id {trip_id} not found")
+    trip.destination    = request.destination
+    trip.days           = request.days
+    trip.budget         = request.budget
+    trip.category       = category
+    trip.daily_budget   = daily_budget
+    db.commit()
+    db.close()
+    
+    return trip
 
 @app.get('/api/v1/trip-categories')
 def get_trip_categories():
